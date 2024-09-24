0402b94bf526e36576ffe03cf6ce71846193c966
Can Sucuoğlu <cansucuoglu@gmail.com>
2024-09-24 16:30:32 -0400

    Collusion Data Updates
    Population Normalization

--- access-mapbox_tilesets/script_2.js
+++ access-mapbox_tilesets/script_2.js
@@ -1,11 +1,7 @@
-
-
 let styleUrl = 'mapbox://styles/hudsonaccess/cm0si1ohd02vk01qr8tgj1g8q'
 
 let mapbox_access_token = 'pk.eyJ1IjoiaHVkc29uYWNjZXNzIiwiYSI6ImNsbDJqbnJ5ZjIyYWgzZHBrNTFwNmp5eHgifQ.WmHbnTbEIrqqpzFU0HF9eA';
-
 mapboxgl.accessToken = mapbox_access_token;
-
 
 const map = new mapboxgl.Map({
     container: 'map',
@@ -18,27 +14,12 @@ const map = new mapboxgl.Map({
 let link = `https://api.mapbox.com/styles/v1/hudsonaccess/cm0si558602t401qq85gsamhk?access_token=${mapbox_access_token}`;
 console.log(link);
 
-
 map.on('load', function() {
 
     let layers = map.getStyle().layers;
-
     console.log(layers);
 
-    let makeVisizble = [
-        'hap-sites-name-program-contac-78n54l (1)',
-        'csos-near-hap-sites-06112024-528lbh'
-    ]
-
-    makeVisizble.forEach(layer => {
-        if (layers.find(l => l.id === layer)) {
-            map.setLayoutProperty(layer, 'visibility', 'visible');
-        }
-    });
-
 });
-
-
 
 // add vector tile 
 console.log( 'Loading the tile layers' )
